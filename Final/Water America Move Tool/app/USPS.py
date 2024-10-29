d34cfcd5dc1b26c0bf2e46a7d0027e5b367b6bfc
Robert Gorman <rbtgorman@gmail.com>
2024-10-29 09:39:16 -0400

    add fix street address code on usps.py

--- Final/Water America Move Tool/app/USPS.py
+++ Final/Water America Move Tool/app/USPS.py
@@ -1,7 +1,6 @@
 import urllib.request
 import xml.etree.ElementTree as ET
 
-
 class AddressValidator():
     def __init__(self, Street_Address2, City, State, Zip5):
         self.Street_Address2 = Street_Address2
@@ -10,88 +9,98 @@ class AddressValidator():
         self.Zip5 = Zip5
 
     def __repr__(self): 
-        return '(%s, %s, %s, %s)' % (self.Address2, self.City, self.State, self.Zip5)
+        return f'({self.Street_Address2}, {self.City}, {self.State}, {self.Zip5})'
 
     def __str__(self):
-        return '(%s, %s, %s, %s)' % (self.Address2, self.City, self.State, self.Zip5)
+        return self.__repr__()
 
     def generate_parse_tree(self):
         Address1 = None
       
-        address_list = []
-        address_list.append(self.Street_Address2.upper())
-        address_list.append(self.City.upper())
-        address_list.append(self.State.upper())
-        address_list.append(self.Zip5.upper())
+        address_list = [
+            self.Street_Address2.upper(),
+            self.City.upper(),
+            self.State.upper(),
+            self.Zip5.upper()
+        ]
 
-        requestXML = '''
+        requestXML = f'''
         <?xml version="1.0"?>
         <AddressValidateRequest USERID="487RUTGE6377">
             <Revision>1</Revision>
             <Address ID="0">
-                <Address1>{}</Address1>
-                <Address2>{}</Address2>
-                <City>{}</City>
-                <State>{}</State>
-                <Zip5>{}</Zip5>
+                <Address1>{Address1}</Address1>
+                <Address2>{self.Street_Address2}</Address2>
+                <City>{self.City}</City>
+                <State>{self.State}</State>
+                <Zip5>{self.Zip5}</Zip5>
             <Zip4/>
             </Address>
         </AddressValidateRequest>
-        '''.format(Address1, self.Street_Address2, self.City, self.State, self.Zip5)
+        '''
 
-         #Prepare.xml.string.doc.for.query.string
-        docString = requestXML
-        docString = docString.replace('\n','').replace('\t','')
+        docString = requestXML.replace('\n','').replace('\t','')
         docString = urllib.parse.quote_plus(docString)
-        url="http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=" + docString
-        #print(url+"\n\n")
+        url = "http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=" + docString
 
-        #API CALL and Check for Errors
         response = urllib.request.urlopen(url)
         if response.getcode() != 200:
             print("Error making HTTP call:")
             print(response.info())
-            exit()
+            return None, None
 
-        #If all went well, we get the content of the response and print it out
         contents = response.read()
-        #print(contents)
-        
         return address_list, ET.fromstring(contents)
 
     def validate_street_address(self):
         address_list, root = self.generate_parse_tree()
+        if root is None:
+            return False
         for address in root.findall('Address'):
-            street_address = (address.find("Address2").text)
-        return (address_list[0] == street_address)
+            street_address = address.find("Address2")
+            if street_address is not None and street_address.text:
+                return address_list[0] == street_address.text
+        return False
             
     def validate_city(self):
         address_list, root = self.generate_parse_tree()
+        if root is None:
+            return False
         for address in root.findall('Address'):
-            city = (address.find("City").text)
-        return (address_list[1] == city)
+            city = address.find("City")
+            if city is not None and city.text:
+                return address_list[1] == city.text
+        return False
 
     def validate_state(self):
         address_list, root = self.generate_parse_tree()
+        if root is None:
+            return False
         for address in root.findall('Address'):
-            state = (address.find("State").text)
-        return (address_list[2] == state)
+            state = address.find("State")
+            if state is not None and state.text:
+                return address_list[2] == state.text
+        return False
 
     def validate_zip_code(self):
         address_list, root = self.generate_parse_tree()
+        if root is None:
+            return False
         for address in root.findall('Address'):
-            zip_code = (address.find("Zip5").text)
-        return (address_list[3] == zip_code)
+            zip_code = address.find("Zip5")
+            if zip_code is not None and zip_code.text:
+                return address_list[3] == zip_code.text
+        return False
     
     def validate_address(self):
         return self.validate_street_address() and self.validate_city() and self.validate_state() and self.validate_zip_code()
 
-#Calling and Testing the class - expecting False
-WrongAddress = AddressValidator("12 Stern Light Drive","Camden","NJ","08054")
-WrongAddress.validate_address()
+# Only run tests if this script is run directly
+if __name__ == "__main__":
+    # Test with incorrect address
+    wrong_address = AddressValidator("12 Stern Light Drive", "Camden", "NJ", "08054")
+    print(f"Wrong Address Validation: {wrong_address.validate_address()}")
 
-#Calling and Testing the class - expecting True
-CorrectAddress = AddressValidator("12 Stern Light Dr","Mount Laurel","NJ","08054")
-CorrectAddress.validate_address()
-
-#SIDE NOTE: it has to be DR not Drive to get True !
+    # Test with correct address
+    correct_address = AddressValidator("12 Stern Light Dr", "Mount Laurel", "NJ", "08054")
+    print(f"Correct Address Validation: {correct_address.validate_address()}")
